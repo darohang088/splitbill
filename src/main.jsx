9806640fc9a2c0ab6024b7f7833a6bc3668f1b8f
DaroHang <56570369+darohang088@users.noreply.github.com>
2026-04-12 20:00:59 +0700

    update screen

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 // import Bill from './bill.jsx'
-import Bill from "./bill_update.jsx";
+// import Bill from "./bill_update.jsx";
+import Bill from "./bill_update_v1.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
