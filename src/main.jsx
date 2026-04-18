41f1f8c85f786767f70c7cadb2e1d406c93b9448
DaroHang <56570369+darohang088@users.noreply.github.com>
2026-04-18 19:13:07 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from "./App.jsx";
 // import Bill from "./bill_update.jsx";
 // import Bill from "./bill_update_v1.jsx";
 // import Bill from "./bill_update_v1_contect.jsx";
-import Bill from "./fix_color.jsx";
+// import Bill from "./fix_color.jsx";
+import Bill from "./update_screenshot.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
